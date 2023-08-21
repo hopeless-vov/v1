d1b16e0dfe7dc028ab1eb0418361282ac3ade1b6
hopeless-vov <vip.vova556@gmail.com>
2023-08-21 11:07:57 +0200

    Add husky

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css';
+import PropTypes from 'prop-types';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+App.propTypes = {
+  Component: PropTypes.any.isRequired,
+  pageProps: PropTypes.exact({
+    pageProps: PropTypes.any,
+  }),
+};
