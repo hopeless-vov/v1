88823582a18d06bc66e97507a5bdde13656aa9a4
hopeless-vov <vip.vova556@gmail.com>
2023-08-22 10:55:22 +0200

    Fix shining height

--- components/shining.js
+++ components/shining.js
@@ -5,7 +5,7 @@ export default function Shining() {
 
   return (
     <div
-      className='pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute'
+      className='pointer-events-none fixed hidden lg:block inset-0 z-30 transition duration-300'
       style={{
         background: `radial-gradient(600px at ${x}px ${y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
       }}
