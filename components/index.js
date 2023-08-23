7703d6418f97425313583029d32f692621f8ee6d
hopeless-vov <vip.vova556@gmail.com>
2023-08-23 14:31:11 +0200

    Add styling for experience section

--- components/index.js
+++ components/index.js
@@ -4,5 +4,5 @@ export { default as Header } from './header';
 export { default as Navigation } from './navigation/navigation';
 export { default as Social } from './socialList/socialList';
 export { default as About } from './sections/about';
-export { default as Experience } from './sections/experience';
+export { default as Experience } from './sections/experience/experience';
 export { default as Projects } from './sections/projects';
---
+++ components/sections/experience/experience.js
@@ -0,0 +1,27 @@
+import ExperienceCard from './experienceCard';
+import { experience } from '@/public/helpers';
+
+export default function Experience() {
+  const { jobs } = experience;
+  console.log('jobs', jobs);
+  return (
+    <section id='experience'>
+      {/* <div>Expirience</div> */}
+      <div>
+        <ol className='group/list'>
+          {jobs.map((job, index) => (
+            <ExperienceCard key={index} job={job} />
+          ))}
+        </ol>
+      </div>
+    </section>
+  );
+}
+
+// Experience.propTypes = {
+//   children: PropTypes.node,
+// };
+
+// Experience.defaultProps = {
+//   children: null,
+// };
