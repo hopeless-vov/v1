66817bbe858362a1c54ea62344fa7eed038cabdf
hopeless-vov <vip.vova556@gmail.com>
2023-08-25 12:44:36 +0200

    Fix shining position on mobile

--- hooks/useMousePosition.js
+++ hooks/useMousePosition.js
@@ -1,12 +1,15 @@
 import React from 'react';
 const useMousePosition = () => {
+  const breakpoint = 1024
   const [mousePosition, setMousePosition] = React.useState({
     x: null,
     y: null,
   });
   React.useEffect(() => {
-    const updateMousePosition = (ev) => {
-      setMousePosition({ x: ev.clientX, y: ev.clientY });
+    const updateMousePosition = ({clientX, clientY}) => {
+      const {innerWidth} = window
+      const lessThanBrealpoint = innerWidth < breakpoint
+      setMousePosition({ x: lessThanBrealpoint ? 0 : clientX, y: lessThanBrealpoint ? 0 : clientY });
     };
     window.addEventListener('mousemove', updateMousePosition);
     return () => {
