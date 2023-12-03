05642b94b7ed514e7c0633281b798f97c0d4048e
hopeless-vov <vov.bndrnk@gmail.com>
2023-12-03 17:08:03 +0100

    Update cv and about me section

--- components/sections/about.js
+++ components/sections/about.js
@@ -3,30 +3,34 @@ export default function About() {
 
   return (
     <section id='about'>
-      <div className='section-heading'>
-        <h2>About</h2>
-      </div>
-      <div>
-        <p className={styling}>
-          Lorem ipsum dolor sit amet. Et quia quisquamAt architecto nam placeat
-          quidem et necessitatibus iste sed perspiciatis vero est totam incidunt
-          ut sint nemo. Id molestiae galisum ut omnis atqueEt esse ut eius
-          distinctio ut quia aperiam? Et eveniet voluptatem sed debitis illum Et
-          laboriosam. Nam omnis consequaturEt praesentium ut similique doloribus
-          et ipsum iure.
-        </p>
-        <p className={styling}>
-          Aut sunt reprehenderitNon temporibus est reiciendis laudantium est
-          explicabo nihil aut quasi omnis. Et aliquid quiaUt voluptate eos
-          laboriosam aperiam eos harum maxime est dignissimos corrupti!
-        </p>
-        <p className={styling}>
-          Qui aperiam harumEt quidem et perferendis veritatis sed recusandae
-          dicta et odio dolor! Non odit iure Est similique et inventore
-          necessitatibus. Vel quam optio et aspernatur pariaturNon illum aut
-          quaerat explicabo et autem quas aut excepturi fugit.
-        </p>
-      </div>
-    </section>
+    <div className='section-heading'>
+      <h2>About</h2>
+    </div>
+    <div>
+      <p className={styling}>
+        Back in 2018, I decided to try my hand at creating web interfaces, and
+        plunged headfirst into the world of web development. Back to today,
+        I'm developing software for my company, trying to develop my
+        leadership qualities, learning new technologies, making cool apps and
+        trying to get to know the world of technology better.
+      </p>
+      <p className={styling}>
+        My main focus these days is building products with the{' '}
+        <a
+          href='https://www.worthsystems.com/'
+          target='_blank'
+          rel='noreferrer'
+          className='inline-flex font-medium transition-all text-slate-200  hover:text-teal-300 focus-visible:text-teal-300 text-base items-center group/link'
+        >
+          Worth
+        </a>{' '}
+        for our customers.
+      </p>
+      <p className={styling}>
+        In my free time, I usually learn to play the guitar, hang out with my
+        friends, travel or play my favorite Video Games.
+      </p>
+    </div>
+  </section>
   );
 }
